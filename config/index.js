87a4a196d105a56751965501e998a780cff0a8c8
Dimitris Stoikidis <dimitrios.stoikidis@sky.uk>
2020-10-20 09:47:14 +0100

    Fixes

--- config/index.js
+++ config/index.js
@@ -1,35 +1,19 @@
 const dotenv = require('dotenv');
 
-// Set the NODE_ENV to 'development' by default
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 const envFound = dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
 
 if (envFound.error) {
-  // This error should crash whole process
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+  throw new Error(`⚠️  Couldn't find ./.env.${process.env.NODE_ENV} file  ⚠️`);
 }
 
 module.exports = {
   [process.env.NODE_ENV]: true,
   env: process.env.NODE_ENV,
-  /**
-   * Your favorite port
-   */
+  debug: process.env.NODE_ENV === "development",
   port: parseInt(process.env.PORT, 10) || 5000,
-
-  /**
-   * That long string from mlab
-   */
   databaseURL: process.env.MONGODB_URI,
-
-  /**
-   * Your secret sauce
-   */
   jwtSecret: process.env.JWT_SECRET,
-
-  /**
-   * Used by log4js configuration
-   */
   loggingConfig: {
     appenders: { "nodejs-api-ain": { type: "file", filename: "logs/nodejs-api-ain.log" } },
     categories: { default: { appenders: ["nodejs-api-ain"], level: process.env.LOG_LEVEL || 'debug' } }
--- server/database/index.js
+++ server/database/index.js
@@ -4,13 +4,11 @@ module.exports = ({ logger, config }) => {
 
   // Exit application on error
   mongoose.connection.on('error', (err) => {
-    console.log(`MongoDB connection error: ${err}`)
     logger.error(`MongoDB connection error: ${err}`);
     process.exit(1);
   });
 
-  // print mongoose logs in dev env
-  mongoose.set('debug', true);
+  mongoose.set('debug', config.debug);
 
   mongoose.connect(config.databaseURL, {
     keepAlive: 1,
@@ -18,13 +16,10 @@ module.exports = ({ logger, config }) => {
     useUnifiedTopology: true
   });
 
-  // console.log(`Mongoose default connection is open to ${uri}`);
   return new Promise((resolve, reject) => {
     mongoose.connection.on('connected', () => {
       logger.info(`Mongoose default connection is open to ${config.databaseURL}`);
-      console.log(`Mongoose default connection is open to ${config.databaseURL}`);
       resolve(mongoose.connection)
     });
   });
-
 };
